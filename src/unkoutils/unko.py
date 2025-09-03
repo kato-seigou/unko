72ca6d4d55fcebde819447667d2803f69afe9689
kato-seigou <seigou444@gmail.com>
2025-09-03 12:09:34 +0900

    add add_unko on unko

--- src/unkoutils/unko.py
+++ src/unkoutils/unko.py
@@ -1,6 +1,6 @@
 def unko():
     return "unko"
 
-def add(a: float, b: float) -> float:
+def add_unko(a: float, b: float) -> float:
     return f"unko_{a + b}"
 
---
+++ test.py
@@ -0,0 +1,3 @@
+from unkoutils import unko
+
+print(unko())
